40249e7a37f687168dd03feea6ae4a40dccff278
R.Gokulprasath <79097212+Gokulprasath2002@users.noreply.github.com>
2022-06-24 09:35:33 +0530

    Update conditionalrendering.js

--- src/conditionalrendering.js
+++ src/conditionalrendering.js
@@ -4,10 +4,10 @@ import Information from './components/Form'
 function Sample(props)
 {
     var x=props.name
-    if(x==='react'){
+    if(x==='React'){
         return(<Information hi={x}/>)
     }
     
 }
 const root=ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Sample name="react"/>)
+root.render(<Sample name="React"/>)
